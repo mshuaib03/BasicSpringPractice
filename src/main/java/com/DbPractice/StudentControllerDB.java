62e25f8af68897ffe33c2f2eaee8456611c739c9
mshuaib03 <shuaib.iitm@gmail.com>
2018-10-08 23:55:32 +0530

    xml & JSON content-type request & response

--- src/main/java/com/DbPractice/StudentControllerDB.java
+++ src/main/java/com/DbPractice/StudentControllerDB.java
@@ -1,15 +1,18 @@
 package com.DbPractice;
+import java.awt.PageAttributes.MediaType;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +21,9 @@ public class StudentControllerDB {
 	@Autowired
 	public StudentServiceDB studentService;
 	
-	@GetMapping("/students")
+	@RequestMapping(value="/students",method=RequestMethod.GET,produces={"application/json","application/xml"})
+	//@RequestMapping(value="/students",method=RequestMethod.GET,produces=MediaType.APPLICATION_XML_VALUE)
+	@ResponseBody
 	public List<StudentDB> getAll(){
 		return studentService.getAllStudents();
 	}
@@ -36,7 +41,7 @@ public class StudentControllerDB {
 		return studentService.DeleteStudent(id);
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT,value="/students/{id}")
+	@RequestMapping(method=RequestMethod.PUT,value="/students/{id}",consumes={"application/json","application/xml"})
 	public String updateStudent(@RequestBody StudentDB s,@PathVariable int id){
 		return studentService.updateStudent(s,id);
 	}
